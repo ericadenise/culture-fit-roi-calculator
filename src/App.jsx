387e7e647ccbaa0bf94026b9916142ae18732bb4
ericadenise <erica@coevolutionproject.com>
2025-05-06 22:18:38 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Culture Fit ROI Calculator</h1>
+      <p>Your app is up and running!</p>
+    </div>
+  );
+}
+
+export default App;
